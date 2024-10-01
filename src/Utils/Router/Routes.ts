a4c24c33c0c7e0d604ea25c189724b97659c0d06
Sara Borges <sarakborges@outlook.com>
2024-09-30 23:08:46 -0300

    changed how page title changes

--- src/Utils/Router/Routes.ts
+++ src/Utils/Router/Routes.ts
@@ -1,5 +1,7 @@
 import { RouteRecordRaw } from 'vue-router'
 
+import { APP_NAME } from '@/Utils/Texts/App.texts'
+
 import { ROUTE_NAMES } from '@/Utils/Data/RouteNames.data'
 
 import HomePage from '@/Pages/Home/Home.page.vue'
@@ -9,19 +11,31 @@ import ProfilePage from '@/Pages/Profile/Profile.page.vue'
 export const HOME: RouteRecordRaw = {
   path: '/',
   name: ROUTE_NAMES.HOME,
-  component: HomePage
+  component: HomePage,
+
+  beforeEnter: () => {
+    document.title = `${APP_NAME} - ${ROUTE_NAMES.HOME}`
+  }
 }
 
 export const LOGIN: RouteRecordRaw = {
   path: '/login',
   name: ROUTE_NAMES.LOGIN,
-  component: LoginPage
+  component: LoginPage,
+
+  beforeEnter: () => {
+    document.title = `${APP_NAME} - ${ROUTE_NAMES.LOGIN}`
+  }
 }
 
 export const PROFILE: RouteRecordRaw = {
   path: '/profile/:url',
   name: ROUTE_NAMES.PROFILE,
-  component: ProfilePage
+  component: ProfilePage,
+
+  beforeEnter: (to) => {
+    document.title = `${APP_NAME} - @${to.params.url}`
+  }
 }
 
 export default [HOME, LOGIN, PROFILE]
